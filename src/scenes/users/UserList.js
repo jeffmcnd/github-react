0fffcd061d7fecc40357a24b9c2f8d00852bc8ae
Jeffrey McNally-Dawes <jeffmcnallydawes@gmail.com>
2017-11-26 23:14:20 -0800

    add network all to Github API
    This app now brings in live data fromt the Github API!

--- src/scenes/users/UserList.js
+++ src/scenes/users/UserList.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 
-// import $ from 'jquery';
+import $ from 'jquery';
+
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/from';
+import 'rxjs/add/observable/fromPromise';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/mergeMap';
 
 import {List} from 'material-ui/List';
 
 import UserRow from './UserRow';
-import usersJson from './../../users.json';
 
 class UserList extends Component {
   constructor(props) {
@@ -21,7 +23,8 @@ class UserList extends Component {
   }
 
   getUsers() {
-    Observable.from(usersJson)
+    Observable.fromPromise($.get("https://api.github.com/users"))
+    .flatMap((users) => Observable.from(users))
     .map(user => <UserRow key={user.id} user={user} />)
     .subscribe((user) => {
       this.setState((prevState, props) => ({
