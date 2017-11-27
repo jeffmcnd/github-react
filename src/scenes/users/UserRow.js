00ed9c3c3167b779a3db9653fe0667a93c409c0a
Jeffrey McNally-Dawes <jeffmcnallydawes@gmail.com>
2017-11-26 23:15:40 -0800

    remove custom CSS for UserRow, using Material UI

--- src/scenes/users/UserRow.js
+++ src/scenes/users/UserRow.js
@@ -3,8 +3,6 @@ import React, { Component } from 'react';
 import Avatar from 'material-ui/Avatar';
 import {ListItem} from 'material-ui/List';
 
-import './UserRow.css';
-
 class UserRow extends Component {
   render() {
     const avatarUrl = this.props.user.avatar_url;
